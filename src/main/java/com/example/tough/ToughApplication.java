573f4c4789ceeb082b91938b580568968481b154
Tough <xiamuyihui@163.com>
2018-03-15 16:38:27 +0800

    创建一个项目

---
+++ src/main/java/com/example/tough/ToughApplication.java
@@ -0,0 +1,12 @@
+package com.example.tough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToughApplication.class, args);
+	}
+}
